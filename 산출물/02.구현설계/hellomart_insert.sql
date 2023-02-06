f03c9def13eb062493a74d5bcb10a1b86d3c048b
sdongpil <pildong94@naver.com>
2023-02-06 17:49:55 +0900

    test: cartDao

--- 산출물/02.구현설계/hellomart_insert.sql
+++ 산출물/02.구현설계/hellomart_insert.sql
@@ -8,7 +8,9 @@ insert into userinfo(userid,password,name,email) values('guard3','3333','김경�
 
 
 /**********************cart insert************************/
-
+insert into cart(cart_no,userid,p_no,cart_qty) values(cart_cart_no_SEQ.nextval,'guard1',2,1);
+insert into cart(cart_no,userid,p_no,cart_qty) values(cart_cart_no_SEQ.nextval,'guard2',1,2);
+insert into cart(cart_no,userid,p_no,cart_qty) values(cart_cart_no_SEQ.nextval,'guard3',3,1);
 
 /**********************orders insert************************/
 
