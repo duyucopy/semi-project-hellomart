884fd89fd733e826a90ce12a0ce8e03d99ac13e4
조성동 <racoCSD@gmail.com>
2023-02-09 18:12:42 +0900

    ..

--- src/main/java/com/itwill/hellomart/order/Order.java
+++ src/main/java/com/itwill/hellomart/order/Order.java
@@ -55,17 +55,6 @@ public class Order {
 		this.o_price = o_price;
 		this.userId = userId;
 		this.orderItemList = new ArrayList<OrderItem>();
-<<<<<<< HEAD
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-=======
->>>>>>> branch 'main' of https://github.com/2022-11-JAVA-DEVELOPER/web-project-team4-hellomart.git
 	}
 
 	public int getO_no() {
