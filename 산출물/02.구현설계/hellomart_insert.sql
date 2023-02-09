a0fea9e65cd6852adbc3efa42f4b5359a36f79d6
itwill <itwill@DESKTOP-O47F7QV>
2023-02-09 09:47:00 +0900

    ...

--- 산출물/02.구현설계/hellomart_insert.sql
+++ 산출물/02.구현설계/hellomart_insert.sql
@@ -18,20 +18,20 @@ insert into categories values(3,'가구');
 
 /**********************product insert************************/
 --제품번호 이름 가격 사진 제품상세
-insert into product values(product_p_no_SEQ.nextval,'엘지전자 스텐드TV',990000,'defalut.png','자유롭게 이동하며 필요할때 내 곁에 두는 스크린',1);
-insert into product values(product_p_no_SEQ.nextval,'삼성전자 TV',980000,'defalut.png','현실에서 보는 듯한 UHD 모니터',1);
-insert into product values(product_p_no_SEQ.nextval,'삼성전자 양문형 냉장고',1430000,'defalut.png','수납은 넉넉하게,공간은 슬림하게',1);
-insert into product values(product_p_no_SEQ.nextval,'엘지전자 오브제컬렉션 노크온',1700000,'defalut.png','어디에나 자연스럽게 아름답게',1);
-insert into product values(product_p_no_SEQ.nextval,'엘지전자 일반 세탁기',395000,'defalut.png','일반 통돌이 세탁기',1);
-insert into product values(product_p_no_SEQ.nextval,'삼성전자 드럼 세탁기',889000,'defalut.png','더 편히하고 위생적인 관리',1);
+insert into product values(product_p_no_SEQ.nextval,'엘지 스텐드TV',990000,'defalut.png','자유롭게 이동하며 필요할때 내 곁에 두는 스크린',1);
+insert into product values(product_p_no_SEQ.nextval,'삼성 TV',980000,'defalut.png','현실에서 보는 듯한 UHD 모니터',1);
+insert into product values(product_p_no_SEQ.nextval,'삼성 양문형 냉장고',1430000,'defalut.png','수납은 넉넉하게,공간은 슬림하게',1);
+insert into product values(product_p_no_SEQ.nextval,'엘지 오브제컬렉션 노크온',1700000,'defalut.png','어디에나 자연스럽게 아름답게',1);
+insert into product values(product_p_no_SEQ.nextval,'엘지 일반 세탁기',395000,'defalut.png','일반 통돌이 세탁기',1);
+insert into product values(product_p_no_SEQ.nextval,'삼성 드럼 세탁기',889000,'defalut.png','더 편히하고 위생적인 관리',1);
 insert into product values(product_p_no_SEQ.nextval,'애플 아이폰 14',1598000,'defalut.png','갬성있고 간지나는 디자인',2);
 insert into product values(product_p_no_SEQ.nextval,'삼성 갤럭시 S23',889900,'defalut.png','편하고 삼성페이가능',2);
-insert into product values(product_p_no_SEQ.nextval,'삼성프리플로우 컴퓨터 본체',1109000,'defalut.png','그래픽 높은 게임 다돌아감',2);
-insert into product values(product_p_no_SEQ.nextval,'삼보컴퓨터',50000,'defalut.png','지뢰찾기 돌아감',2);
-insert into product values(product_p_no_SEQ.nextval,'삼성전자 갤럭시 북3',3600000,'defalut.png','초슬림 초경량 노트북',2);
-insert into product values(product_p_no_SEQ.nextval,'에플 맥북에어 ',2124000,'defalut.png','초갬성 초슬림 노트북',2);
-insert into product values(product_p_no_SEQ.nextval,'에이스침대',1258280,'defalut.png','편한침대',3);
-insert into product values(product_p_no_SEQ.nextval,'일룸 책상세트',519000,'defalut.png','책상의자세트',3);
+insert into product values(product_p_no_SEQ.nextval,'삼성 프리플로우 컴퓨터 본체',1109000,'defalut.png','그래픽 높은 게임 다돌아감',2);
+insert into product values(product_p_no_SEQ.nextval,'삼보 컴퓨터',50000,'defalut.png','지뢰찾기 돌아감',2);
+insert into product values(product_p_no_SEQ.nextval,'삼성 갤럭시 북3',3600000,'defalut.png','초슬림 초경량 노트북',2);
+insert into product values(product_p_no_SEQ.nextval,'애플 맥북에어 ',2124000,'defalut.png','초갬성 초슬림 노트북',2);
+insert into product values(product_p_no_SEQ.nextval,'에이스 침대',1258280,'defalut.png','편한침대',3);
+insert into product values(product_p_no_SEQ.nextval,'일룸 책상 세트',519000,'defalut.png','책상의자세트',3);
 insert into product values(product_p_no_SEQ.nextval,'리바트 행거',95000,'defalut.png','어린이 옷걸이 행거',3);
 insert into product values(product_p_no_SEQ.nextval,'일룸 로이모노 책상서랍',149000,'defalut.png','예쁜색상서랍',3);
 insert into product values(product_p_no_SEQ.nextval,'필립스 스탠드 조명',119000,'defalut.png','책상 스텐드 조명',3);
